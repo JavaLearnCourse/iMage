0265aa11ebb430874402afcf89221b7f1ab20211
Dmitry Savkin <dmitry.savkin.info@gmail.com>
2020-07-22 20:10:34 +0200

    Coomponent scan

--- springbootmodule/src/main/java/org/WebApplication.java
+++ springbootmodule/src/main/java/org/WebApplication.java
@@ -5,15 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-        "com.webTemplate.config.web",  // MVC @Configuration
-        "com.webTemplate.config.security", // Security @Configuration
-        "com.webTemplate.config.jpa", // Database @Configuration -> does Entity Scan and Repository scan
-        "com.webTemplate.service", // Service scan @Service
-        "com.webTemplate.controler", // Controller scan @Controller
-        "org.webapp",
-        "springbootdemo.src.main.webapp.WEB-INF.jsp"
-})
+@ComponentScan("org.webapp")
 
 public class WebApplication {
 
